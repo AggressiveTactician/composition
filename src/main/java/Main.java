3b0cc644387f81783719b057a577a9db54ac31b7
Użytkownik <uytkownik@kursGIT.pl>
2020-08-01 12:47:44 +0200

    Final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ComputerFactory factory = new ComputerFactory();
-        Computer PC = factory.createComputer("CPU", "GPU", "PSU");
+        Computer PC = factory.createComputer("i5 9600k", "2070S", "CTG 750C");
         System.out.println(PC.description());
     }
 }
